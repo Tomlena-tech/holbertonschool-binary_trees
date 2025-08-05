758154fef028f85b7ca0f169d17399935abdb79c
Ilnnn <camoreze@gmail.com>
2025-08-05 09:40:38 +0000

    betty correction of all task files 1 3 5

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,26 +7,27 @@
  * Return: A pointer to created node or NULL on failure.
  */
 
- binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
- {
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+
 binary_tree_t *new_node;
 new_node = NULL;
 
-    if (parent == NULL && new_node == NULL)
-    {
-        return (NULL);
-    }
+	if (parent == NULL && new_node == NULL)
+	{
+		return (NULL);
+	}
 
-    new_node = binary_tree_node(parent, value);
+	new_node = binary_tree_node(parent, value);
 
-    if (parent->right != NULL)
-    {
-        new_node->right = parent->right;
-        parent->right->parent = new_node;
-    }
-    
-    parent->right = new_node;
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
 
-    return (new_node);
+	parent->right = new_node;
 
- }
+	return (new_node);
+
+}
