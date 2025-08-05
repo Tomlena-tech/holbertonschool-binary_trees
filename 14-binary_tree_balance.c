c33443f4be243084b4d3ee3a3f96cfe736a9d975
Tomlena-tech <decourttthomas@gmail.com>
2025-08-05 14:43:07 +0200

    test add 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,35 +1,48 @@
-
 #include "binary_trees.h"
 
-/**
- * binary_tree_balance - Measures the balance factor of a binary tree.
- * @tree: A pointer to the root node of the tree to measure the balance factor.
- *
- * Return: If tree is NULL, return 0, else return balance factor.
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	if (tree)
-		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
-
-	return (0);
-}
 
 /**
- * binary_tree_height - Measures the height of a binary tree.
- * @tree: A pointer to the root node of the tree to measure the height.
- *
- * Return: If tree is NULL, your function must return 0, else return height.
+ * binary_tree_height - Calcule la hauteur d’un arbre binaire.
+ * @tree: Pointeur vers la racine de l’arbre.
+ * Return: Hauteur de l’arbre, ou 0 si NULL.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		size_t l = 0, r = 0;
+    size_t left_height, right_height;
 
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-		return ((l > r) ? l : r);
-	}
-	return (0);
+    if (tree == NULL)
+        return (0);
+
+    if (tree->left == NULL && tree->right == NULL)
+        return (0);
+
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    if (left_height > right_height)
+        return (left_height + 1);
+    else
+        return (right_height + 1);
+}
+
+
+/**
+ * binary_tree_depth - Calcule la profondeur d’un nœud dans l’arbre.
+ * @node: Pointeur vers le nœud dont on veut la profondeur.
+ * Return: Profondeur du nœud, ou 0 si NULL ou racine.
+ */
+size_t binary_tree_depth(const binary_tree_t *node)
+{
+    size_t depth = 0;
+
+    if (node == NULL)
+        return (0);
+
+    while (node->parent != NULL)
+    {
+        depth++;
+        node = node->parent;
+    }
+
+    return (depth);
 }
